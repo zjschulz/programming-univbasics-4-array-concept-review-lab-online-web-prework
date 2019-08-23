818c2ae56f79b77270b49b5e29bf996504dca4f7
Zachary Schulz <zjschulz@gmail.com>
2019-08-23 20:37:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
- counter = 0
+ counter = value_to_find
   while counter < numbers.length do
     square[counter] = numbers[counter] ** 2
     counter += 1
