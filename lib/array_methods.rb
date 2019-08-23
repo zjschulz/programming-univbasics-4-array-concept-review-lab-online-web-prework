33db6a575a8520ae09d5afb87ffde89a39d21606
Zachary Schulz <zjschulz@gmail.com>
2019-08-23 20:37:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-
+while counter < numbers.length do
+    square[counter] = numbers[counter] ** 2
+    counter += 1
+  end
 end
 
 def find_max_value(array)
